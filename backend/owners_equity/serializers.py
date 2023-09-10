6e9ebc52c087f6049c529ed2838bee61bb716ebf
kenthought <nananaupan@gmail.com>
2023-09-11 02:16:31 +0800

    updated owners equity barangay, municipality, province relationship

--- backend/owners_equity/serializers.py
+++ backend/owners_equity/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from .models import OwnersEquity, Ledger
 from components.serializers import (
-    BarangaySerializer,
-    MunicipalitySerializer,
+    BarangayViewSerializer,
+    MunicipalityViewSerializer,
     ProvinceSerializer,
 )
 from users.serializers import UserSerializer
 
 
 class OwnersEquityViewSerializer(serializers.ModelSerializer):
-    barangay = BarangaySerializer()
-    municipality = MunicipalitySerializer()
+    barangay = BarangayViewSerializer()
+    municipality = MunicipalityViewSerializer()
     province = ProvinceSerializer()
     user = UserSerializer()
 
