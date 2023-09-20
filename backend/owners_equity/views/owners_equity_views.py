81b144a0476d670dbe4f40921c8aaca35bdd88fd
kenthought <nananaupan@gmail.com>
2023-09-21 02:19:07 +0800

    code cleanup

--- backend/owners_equity/views/owners_equity_views.py
+++ backend/owners_equity/views/owners_equity_views.py
@@ -10,7 +10,6 @@ from django.db.models import F
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
-import inspect
 
 
 # Create your views here.
