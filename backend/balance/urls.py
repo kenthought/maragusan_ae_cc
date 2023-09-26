eaa19ec2fa177ea181277f3954482b200242820b
kenthought <nananaupan@gmail.com>
2023-09-26 15:56:12 +0800

    Remove ignored files

--- backend/balance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from balance.views import *
-
-urlpatterns = [
-    path("<str:type>/", BalanceDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/user_permissions/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UserPermissions
-
-
-class UserPermissionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserPermissions
-        fields = ["id", "permissions", "user"]
