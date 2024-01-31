067ebf9d7aaff92aa975f97afdd5a1ffaebacc8a
reymart-ingenuity <reymart@ingenuity.ph>
2024-01-31 23:41:56 +0800

    Fixed cache files

--- backend/ledger/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import LedgerCode
-
-
-class LedgerCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LedgerCode
-        fields = ["id", "ledger", "module_id", "code"]
